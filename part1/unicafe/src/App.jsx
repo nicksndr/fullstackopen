029de553f18abdfae8ff044f32c2a3af10b5126d
Nicklas Schneider <nicklas@marken-schneider.de>
2025-01-28 22:56:42 +0100

    ex 1.7 first implementations

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,66 @@
 import { useState } from 'react'
 
+const Header = (props) => {
+  console.log(props)
+  return (
+    <h1>{props.heading}</h1>
+  );
+};
+
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <p>
+    {props.text} {props.eval}
+  </p>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const increaseGoodByOne = () => {
+    console.log('increasing, value before', good)
+    setGood(good + 1)
+  }
+
+  const increaseNeutralByOne = () => {
+    console.log('increasing, value before', neutral)
+    setGood(neutral + 1)
+  }
+
+  const increaseBadByOne = () => {
+    console.log('increasing, value before', bad)
+    setGood(bad + 1)
+  }
+
+  const heading = "give feedback"
+  const heading2 = "statistics"
+
   return (
     <div>
-      code here
+      <Header heading={heading}/>
+
+      <Button onClick={increaseGoodByOne} text="good" />
+      <Button onClick={increaseNeutralByOne} text="neutral" />
+      <Button onClick={increaseBadByOne} text="bad" />
+
+      <Header heading={heading2}/>
+      <Content eval={good} text="good"/>
+      <Content eval={good} text="neutral"/>
+      <Content eval={good} text="bad"/>
     </div>
   )
 }

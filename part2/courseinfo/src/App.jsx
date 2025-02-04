c7c8f2dfb3a57c6dea4f982b0f0b8b327362193d
Nicklas Schneider <nicklas@marken-schneider.de>
2025-02-04 23:11:42 +0100

    exercise 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,51 @@
+const Course = (props) => (
+  <div>
+    <Header name={props.course.name} />
+    <Content parts={props.course.parts} />
+  </div>
+);
+
+const Header = (props) => <h1>{props.name}</h1>
+
+
+const Content = (props) => (
+  <div>
+    {props.parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+);
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+);
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App

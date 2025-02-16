7bdfcbcd9d563174a25f9806dfad4d6cfdb0b4f3
Nicklas Schneider <nicklas@marken-schneider.de>
2025-02-16 21:51:37 +0100

    exercise 2.6 part 2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
 
+const Person = ({ person }) => {
+  return (
+    <div>{person.name}</div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
@@ -7,8 +13,16 @@ const App = () => {
   const [newName, setNewName] = useState('')
 
   const addName = (event) => {
+    // prevent default form submission
+    // console.log('button clicked', event.target)
     event.preventDefault()
-    console.log('button clicked', event.target)
+
+    const personObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
   }
 
   const handleNameChange = (event) => {
@@ -19,17 +33,22 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      {/* once submitted the HTML default is prevented and the name is added to the list */}
       <form onSubmit={addName}>
         <div>
+          {/* handles when something is typed to properly display */}
           <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
-          <div>debug: {newName}</div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map(person => 
+          <Person key={person.id} person={person} />
+        )}
+      </div>
     </div>
   )
 }

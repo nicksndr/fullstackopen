249acdb3e14d60275f90866226e06242d7c16b71
Nicklas Schneider <nicklas@marken-schneider.de>
2025-06-16 22:34:42 +0200

    exercise 2.16

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import personService from './services/persons'
+import './index.css'
 
 const Filter = ({ filterName, handleFilterChange }) => {
   return (
@@ -47,12 +48,26 @@ const Person = ({ person, removePerson }) => {
   );
 };
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className='success'>
+      {message}
+    </div>
+  )
+}
+
+
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
+  const [successMessage, setSuccessMessage] = useState('added person...')
 
   // Fetch data from JSON server when the component loads
   useEffect(() => {
@@ -105,6 +120,13 @@ const App = () => {
       setPersons(persons.concat(newPerson));
       setNewName('');
       setNewNumber('');
+
+      setSuccessMessage(
+        `Added '${newName}'`
+      )
+      setTimeout(() => {
+        setSuccessMessage(null)
+      }, 5000)
     })
     .catch(error => {
       console.error("Error adding person:", error);
@@ -128,6 +150,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={successMessage} />
       <Filter filterName={filterName} handleFilterChange={(e) => setFilterName(e.target.value)} />
       <h3>Add a new</h3>
       <PersonForm 

9ab462b10499a65ad778f75631e8614f2685f334
Nicklas Schneider <nicklas@marken-schneider.de>
2025-07-06 22:16:28 +0200

    exercise 2.20

--- part2/weatherapp/src/App.jsx
+++ part2/weatherapp/src/App.jsx
@@ -5,8 +5,40 @@ const App = () => {
   const [query, setQuery] = useState('')
   const [countries, setCountries] = useState([])
   const [selectedCountry, setSelectedCountry] = useState(null)
+  const [weather, setWeather] = useState([])
+
+  const api_key = import.meta.env.VITE_WEATHER_API_KEY
+
+  // code here runs every time selectedCountry changes
+  useEffect(() => {
+    console.log(selectedCountry)
+    if (!selectedCountry) return
+  
+    const capital = selectedCountry.capital?.[0]
+    console.log(capital)
+    if (!capital) return
+  
+    axios
+      .get(`https://api.openweathermap.org/geo/1.0/direct?q=${capital}&limit=1&appid=${api_key}`)
+      .then(geoResponse => {
+        const { lat, lon } = geoResponse.data[0]
+
+        console.log(geoResponse)
+        console.log(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&units=metric&appid=${api_key}`)
+  
+        return axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&units=metric&appid=${api_key}`)
+      })
+      .then(weatherResponse => {
+        console.log("Weather API response:", weatherResponse.data)
+        setWeather(weatherResponse.data.daily)
+      })
+      .catch(error => {
+        console.error("Error fetching weather data:", error)
+      })
+  }, [selectedCountry, api_key])
 
   useEffect(() => {
+    setWeather([])
     if (query) {
       axios
         .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
@@ -45,6 +77,17 @@ const App = () => {
         alt={`Flag of ${country.name.common}`}
         style={{ width: '150px' }}
       />
+    {weather.length > 0 && (
+      <>
+        <h2>Weather in {country.capital?.[0]}</h2>
+        {/* You need to use [0] because the daily weather data returned by the OpenWeather API is an array 
+        The question mark (?.) is called the optional chaining operator in JavaScript.
+        It prevents your code from crashing if a value is undefined or null.*/}
+        <p>Temperature: {weather[0].temp.day} °C</p>
+        <p>Wind: {weather[0].wind_speed} m/s</p>
+        <img src={`https://openweathermap.org/img/wn/${weather[0].weather[0].icon}@2x.png`} alt="Weather icon" />
+      </>
+    )}
       </div>
     )
   }
@@ -70,7 +113,7 @@ const App = () => {
         )}
 
 
-        {countries.length === 1 && showCountry(countries[0])}
+        {countries.length === 1 && !selectedCountry && setSelectedCountry(countries[0])}
 
         {selectedCountry && showCountry(selectedCountry)}
       </ul>

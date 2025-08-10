7de99932cac1525cf76bf91a887bbdc2eaa91f07
Nicklas Schneider <nicklas@marken-schneider.de>
2025-08-10 23:07:13 +0200

    exercise 3.2

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const PORT = 3001
 
 app.use(express.json())
 
@@ -10,11 +11,19 @@ let persons = [
     { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" }
   ]
 
+  //all the persons
   app.get('/api/persons', (request, response) => {
     response.json(persons)
   })
 
-const PORT = 3001
+  // info page
+  app.get('/info', (req, res) => {
+    const entriesCount = persons.length
+    const requestTime = new Date().toString()  
+    res.send(`Phonebook has info fo ${entriesCount} people <br> Request received at: ${requestTime}`)
+    //res.send('Info page is working')
+  })
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })

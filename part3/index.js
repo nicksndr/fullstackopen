1a7628ead8eaa23e33d66c7cb37aa67100dc2064
Nicklas Schneider <nicklas@marken-schneider.de>
2025-09-30 23:37:50 +0200

    exercise 3.9

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors()) // <--- enable CORS for all routes
 
 let persons = [
     { id: "1", name: "Arto Hellas", number: "040-123456" },

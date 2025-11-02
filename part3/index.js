db367db8b6afcbf88a6eee0e1d6a0ff9780b9351
Nicklas Schneider <nicklas@marken-schneider.de>
2025-11-02 18:47:14 +0100

    dynamic port

--- part3/index.js
+++ part3/index.js
@@ -131,7 +131,7 @@ app.get('/api/persons/:id', (request , response) => {
   }
   app.use(errorHandler)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })

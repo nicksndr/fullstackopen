57417b5123fd11177e0a77ad452e50a468afd73d
Nicklas Schneider <nicklas@marken-schneider.de>
2025-11-08 16:41:07 +0100

    exercise 4.4

--- part4/tests/blogApp.test.js
+++ part4/tests/blogApp.test.js
@@ -8,3 +8,20 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+describe('totalLikes', () => {
+    const listWithOneBlog = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+          likes: 5,
+          __v: 0
+        }
+      ]
+    test('total sum of likes in all of the blog posts', () => {
+        const result = listHelper.totalLikes(listWithOneBlog)
+        assert.strictEqual(result, 5)
+    })
+  })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
     return 1
   }
+
+  const totalLikes = (blogs) => {
+    let sumLikes = 0
+    blogs.forEach(blog => {
+        sumLikes += blog.likes
+    });
+    return sumLikes
+  }
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }

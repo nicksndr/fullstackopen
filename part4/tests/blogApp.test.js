125076eb8b35da5dff0163506fb1e7292070b84b
Nicklas Schneider <nicklas@marken-schneider.de>
2025-11-08 19:31:25 +0100

    exercise 4.6

--- part4/tests/blogApp.test.js
+++ part4/tests/blogApp.test.js
@@ -25,3 +25,103 @@ describe('totalLikes', () => {
         assert.strictEqual(result, 5)
     })
   })
+
+  describe('favoriteBlog', () => {
+    const listWithBlogs = [
+        {
+            _id: '1',
+            title: 'Title 1',
+            author: 'Author 1',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 4,
+            __v: 0
+        },
+        {
+            _id: '2',
+            title: 'Title 2',
+            author: 'Author 2',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 3,
+            __v: 0
+        },
+        {
+            _id: '3',
+            title: 'Title 3',
+            author: 'Author 3',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 6,
+            __v: 0
+        }
+        
+      ]
+    test('blog with the most likes', () => {
+        const result = listHelper.favoriteBlog(listWithBlogs)
+        assert.deepStrictEqual(
+            result,
+            {
+                _id: '3',
+                title: 'Title 3',
+                author: 'Author 3',
+                url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+                likes: 6,
+                __v: 0
+            }
+        )
+    })
+  })
+
+  describe('mostBlogs', () => {
+    const blogs = [
+        {
+          _id: '1',
+          title: 'Understanding Node.js',
+          author: 'Alice Johnson',
+          url: 'https://example.com/node',
+          likes: 7,
+          __v: 0
+        },
+        {
+          _id: '2',
+          title: 'Mastering Express',
+          author: 'Bob Smith',
+          url: 'https://example.com/express',
+          likes: 5,
+          __v: 0
+        },
+        {
+          _id: '3',
+          title: 'MongoDB Tips',
+          author: 'Alice Johnson',
+          url: 'https://example.com/mongo',
+          likes: 3,
+          __v: 0
+        },
+        {
+          _id: '4',
+          title: 'Full Stack Best Practices',
+          author: 'Charlie Lee',
+          url: 'https://example.com/fullstack',
+          likes: 10,
+          __v: 0
+        },
+        {
+          _id: '5',
+          title: 'Async JavaScript',
+          author: 'Alice Johnson',
+          url: 'https://example.com/async',
+          likes: 8,
+          __v: 0
+        }
+      ]
+      
+    test('author with most blogs', () => {
+        const result = listHelper.mostBlogs(blogs)
+        assert.deepStrictEqual(
+            result,
+            {
+                author: "Alice Johnson",
+                blogs: 3
+            }
+        )
+    })
+  })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
   }
@@ -9,8 +11,30 @@ const dummy = (blogs) => {
     });
     return sumLikes
   }
+
+  const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) return null
+    
+    let mostLikes = blogs[0]
+    blogs.forEach(blog => {
+        if (blog.likes > mostLikes.likes) 
+            mostLikes = blog
+      })
+    return mostLikes
+  }
+
+  const mostBlogs = (blogs) => {
+    const counts = _.groupBy(blogs, 'author') // { 'Alice': 3, 'Bob': 2, ... }
+    const authors = _.map(counts, (blogsArray, author) => ({
+        author: author,
+        blogs: blogsArray.length
+      }))
+    return _.maxBy(authors, 'blogs') || null
+  }
   
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs
   }

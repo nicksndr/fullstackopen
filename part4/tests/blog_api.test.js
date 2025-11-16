bb1293689ce84cb09859970abca270066846bd05
Nicklas Schneider <nicklas@marken-schneider.de>
2025-11-16 14:47:35 +0100

    exercise 4.8 part 3

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -9,16 +9,33 @@ const config = require('../utils/config')
 
 const api = supertest(app)
 
+before(async () => {
+  await mongoose.connect(config.MONGODB_URI)
+})
+
 beforeEach(async () => {
   await Blog.deleteMany({})
   await Blog.insertMany(helper.initialBlogs)
 })
 
-after(async () => {
-  await mongoose.connection.close()
+
+describe('testing the blogs', () => {
+
+  test('should return correct amount of blogs', async () => {
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
+
+  test('should return blogs with right unique identifier property', async () => {
+    const response = await api.get('/api/blogs')
+    response.body.forEach((blog) => {
+      assert.ok(blog.id)
+      assert.strictEqual(blog._id, undefined)
+    })
+  })
+
 })
 
-test('should return correct amount of blogs', async () => {
-  const response = await api.get('/api/blogs')
-  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+after(async () => {
+  await mongoose.connection.close()
 })

4ea870fea478f8db036ebe1f29eb86eb2f2776ad
nicksndr <nicklas@marken-schneider.de>
2025-11-17 21:24:27 +0100

    Refactoring the roures from promises to async await syntax

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,38 +1,34 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const { error } = require('../utils/logger')
 
-blogsRouter.get('/', (request, response) => {
-    Blog.find({}).then((blogs) => {
-      response.json(blogs)
-    })
-  })
+blogsRouter.get('/', async (request, response) => {
+    
+  const blogs = await Blog.find({}) // When using async/await syntax, Express will automatically call the error-handling middleware if an await statement throws an error or the awaited promise is rejected. 
+  response.json(blogs)
+})
   
-  blogsRouter.get('/:id', (request , response) => {
-    Blog.findById(request.params.id)
-      .then(blog => {
-        if (blog) {
-          response.json(blog)
-        } else {
-          response.status(404).end()
-        }
-      })
-      .catch(err => {
-        error('Error finding blog:', err)
-        response.status(400).send({ error: 'malformatted id' })
-      })
+  blogsRouter.get('/:id', async (request , response) => {
+    const blog = await Blog.findById(request.params.id)
+
+    if (blog) {
+      response.json(blog)
+    } else {
+      response.status(404).end()
+    }
   })
 
-  blogsRouter.delete('/:id', (request, response, next) => {
-    Blog.findByIdAndDelete(request.params.id)
-      .then(() => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
+  blogsRouter.delete('/:id', async(request, response, next) => {
+    const deletedBlog = await Blog.findByIdAndDelete(request.params.id)
+    
+    if (deletedBlog) {
+      response.status(204).end()
+    } else {
+      response.status(404).end()
+    }
   })
   
   //update blog post with PUT request
-  blogsRouter.put('/:id', (request, response, next) => {
+  blogsRouter.put('/:id', async(request, response, next) => {
     const body = request.body
   
     const blog = {
@@ -42,21 +38,31 @@ blogsRouter.get('/', (request, response) => {
       likes: body.likes,
     }
   
-    Blog.findByIdAndUpdate(request.params.id, blog, { new: true, runValidators: true })
-      .then((updatedBlog) => {
-        if (updatedBlog) {
-          response.json(updatedBlog)
-        } else {
-          response.status(404).end()
-        }
-      })
-      .catch((err) => {
-        error('Error updating blog:', err)
-        next(err)
-      })
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true, runValidators: true })
+
+    if (updatedBlog) {
+      response.json(updatedBlog)
+    } else {
+      response.status(404).end()
+    }
+
+
+      // Old version with promises
+
+      // .then((updatedBlog) => {
+      //   if (updatedBlog) {
+      //     response.json(updatedBlog)
+      //   } else {
+      //     response.status(404).end()
+      //   }
+      // })
+      // .catch((err) => {
+      //   error('Error updating blog:', err)
+      //   next(err)
+      // })
   })
   
-  blogsRouter.post('/', (request, response) => {
+  blogsRouter.post('/', async(request, response) => {
     const body = request.body
   
     if (!body.title) {
@@ -74,14 +80,8 @@ blogsRouter.get('/', (request, response) => {
       likes: body.likes || 0,
     })
   
-    blog.save()
-      .then((result) => {
-        response.status(201).json(result)
-      })
-      .catch((err) => {
-        error('Error saving blog:', err)
-        response.status(500).json({ error: 'Failed to save blog' })
-      })
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
   })
 
 module.exports = blogsRouter

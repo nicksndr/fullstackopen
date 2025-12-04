574194fbeb056cad46cfd38370134efeeb546713
nicksndr <nicklas@marken-schneider.de>
2025-12-04 12:28:21 +0100

    exercise 5.10 sort blogs by number of likes

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -164,7 +164,9 @@ const App = () => {
       />
     </Togglable>
 
-      {blogs.map(blog =>
+      {blogs
+      .sort((a, b) => b.likes - a.likes)
+      .map(blog =>
         <Blog key={blog.id} blog={blog} onClick={() => likeButton(blog)} />
       )}
     </div>

2539afc252671ae5200a486077990d762fc3ea01
nicksndr <nicklas@marken-schneider.de>
2025-12-04 11:41:42 +0100

    exercise 5.7

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+
+  const [contentVisible, setContentVisible] = useState(false)
+
+  const hideWhenVisible = { display: contentVisible ? 'none' : '' }
+  const showWhenVisible = { display: contentVisible ? '' : 'none' }
+  
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        {blog.title}
+        <button onClick={() => setContentVisible(true)}>view</button>
+      </div>
+
+      <div style={showWhenVisible}>
+        {blog.title}
+        <button onClick={() => setContentVisible(false)}>hide</button><br/>
+        {blog.url}<br/>
+        {"likes "}{blog.likes} <button>like</button><br/>
+        {blog.author}
+      </div>
+    </div>  
+)}
 
 export default Blog

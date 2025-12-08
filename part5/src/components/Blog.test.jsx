61193a7125d260b911e5a68a63dc14ed3cf3c6f8
nicksndr <nicklas@marken-schneider.de>
2025-12-08 22:47:20 +0100

    exercise 5.14 test to check if view button works

--- part5/src/components/Blog.test.jsx
+++ part5/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 const sampleBlog = {
@@ -35,4 +36,24 @@ test('renders title and author but hides url and likes by default', () => {
   expect(url).not.toBeVisible()
   expect(likes).not.toBeVisible()
 
+})
+
+test('after clicking the button, url and like is visible', async () => {
+    render(
+        <Blog
+          blog={sampleBlog}
+          onClick={() => {}}
+          remove={() => {}}
+        />
+      )
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view', { exact: false })
+    await user.click(button)
+
+    const urlElements = screen.getAllByText(sampleBlog.url, { exact: false })
+    const likesElements = screen.getAllByText(`likes ${sampleBlog.likes}`, { exact: false })
+
+    expect(urlElements[0]).toBeVisible()
+    expect(likesElements[0]).toBeVisible()
 })

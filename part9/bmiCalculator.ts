448ffd742a2244a074b7378ae92da66397b44509
nicksndr <nicklas@marken-schneider.de>
2026-01-20 22:08:06 +0100

    exercise 9.1 create typescript function for calculating BMI

---
+++ part9/bmiCalculator.ts
@@ -0,0 +1,29 @@
+const calculateBmi = (height: number, weight: number,) : string => {
+    const bmi = weight / (height * height)
+
+    if(bmi < 18.5) {
+        return 'Underweight'
+    }
+    if(bmi >= 18.5 && bmi <= 24.9) {
+        return 'Normal weight'
+    }
+    if(bmi >= 25 && bmi <= 29.9) {
+        return 'Overweight'
+    }
+    if(bmi >= 30) {
+        return 'Obesity'
+    }
+}
+
+try {
+  console.log(calculateBmi(185, 100))
+  console.log(calculateBmi(185, 70))
+  console.log(calculateBmi(185, 90))
+  console.log(calculateBmi(185, 65))
+} catch (error: unknown) {
+  let errorMessage = 'Something went wrong: '
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}

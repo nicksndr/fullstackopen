a864902d6536b012b52b7f55476a99ea35c90964
nicksndr <nicklas@marken-schneider.de>
2026-01-22 22:05:43 +0100

    exercise 9.3 change code to accept command-line arguments

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,30 +1,62 @@
-const calculateBmi = (height: number, weight: number,) : string => {
+interface bmiValues {
+    value1: number;
+    value2: number;
+}
+
+const parseArguments = (args: string[]): bmiValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            value1: Number(args[2]),
+            value2: Number(args[3])
+        }
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+}
+
+const calculateBmi = (height: number, weight: number,): string => {
     const heightInMeters = height / 100
     const bmi = weight / (heightInMeters * heightInMeters)
 
-    if(bmi < 18.5) {
+    if (bmi < 18.5) {
         return 'Underweight'
     }
-    if(bmi >= 18.5 && bmi <= 24.9) {
+    if (bmi >= 18.5 && bmi <= 24.9) {
         return 'Normal weight'
     }
-    if(bmi >= 25 && bmi <= 29.9) {
+    if (bmi >= 25 && bmi <= 29.9) {
         return 'Overweight'
     }
-    if(bmi >= 30) {
+    if (bmi >= 30) {
         return 'Obesity'
     }
 }
 
 try {
-  console.log(calculateBmi(185, 100))
-  console.log(calculateBmi(185, 70))
-  console.log(calculateBmi(185, 90))
-  console.log(calculateBmi(185, 65))
+    const { value1, value2 } = parseArguments(process.argv);
+    console.log(calculateBmi(value1, value2))
 } catch (error: unknown) {
-  let errorMessage = 'Something went wrong: '
-  if (error instanceof Error) {
-    errorMessage += error.message;
-  }
-  console.log(errorMessage);
+    let errorMessage = 'Something went wrong: '
+    if (error instanceof Error) {
+        errorMessage += error.message;
+    }
+    console.log(errorMessage);
 }
+
+
+
+// try {
+//   console.log(calculateBmi(185, 100))
+//   console.log(calculateBmi(185, 70))
+//   console.log(calculateBmi(185, 90))
+//   console.log(calculateBmi(185, 65))
+// } catch (error: unknown) {
+//   let errorMessage = 'Something went wrong: '
+//   if (error instanceof Error) {
+//     errorMessage += error.message;
+//   }
+//   console.log(errorMessage);
+// }
--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -8,7 +8,28 @@ interface exerciseSummary {
     value7: number;
   }
 
-  const calculateExercises = (args: number[], target: number): exerciseSummary => {
+interface exerciseValues {
+    target: number;
+    days: number[];
+}
+
+const parseArguments = (args: string[]): exerciseValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+
+    const target = Number(args[2]);
+    const days = args.slice(3).map(value => Number(value));
+
+    if (isNaN(target) || days.some(value => isNaN(value))) {
+        throw new Error('Provided values were not numbers!');
+    }
+
+    return {
+        target,
+        days
+    };
+};
+
+  const calculateExercises = (target: number, args: number[]): exerciseSummary => {
     const periodLength = args.length
     const trainingDays = args.filter(day => day > 0).length
     const average = args.reduce((a, b) => a + b, 0) / args.length
@@ -28,13 +49,24 @@ interface exerciseSummary {
     }
 
   }
-  
-  try {
-    console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
-  } catch (error: unknown) {
-    let errorMessage = 'Something went wrong: '
+
+try {
+    const { target, days } = parseArguments(process.argv);
+    console.log(calculateExercises(target, days));
+} catch (error: unknown) {
+    let errorMessage = 'Something went wrong: ';
     if (error instanceof Error) {
-      errorMessage += error.message;
+        errorMessage += error.message;
     }
     console.log(errorMessage);
-  }
+}
+
+//   try {
+//     console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+//   } catch (error: unknown) {
+//     let errorMessage = 'Something went wrong: '
+//     if (error instanceof Error) {
+//       errorMessage += error.message;
+//     }
+//     console.log(errorMessage);
+//   }

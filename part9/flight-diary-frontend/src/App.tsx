02d3981a133a122c9b93f39c5445026e5f3cec74
nicksndr <nicklas@marken-schneider.de>
2026-02-10 22:22:35 +0100

    exerise 9.17 starting setting up the flight diary frontend

--- part9/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary-frontend/src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import type { DiaryEntry } from '../types.ts'
+import diaryService from "./services/diaries.ts";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+
+  useEffect(() => {
+    diaryService.getAll().then((diaries: DiaryEntry[]) => setDiaries(diaries));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Diary Entries</h1>
+      <ul>
+        {diaries.map((diary) => (
+          <div>
+          <h3 key={diary.id}>{diary.date}</h3>
+          <li key={diary.id}>{diary.weather}</li>
+          <li key={diary.id}>{diary.visibility}</li>
+          </div>
+        ))}
+      </ul>
+    </div>
   )
 }
 
---
+++ part9/flight-diary-frontend/src/services/diaries.ts
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import type { DiaryEntry } from '../../types'
+const baseUrl = '/api/diaries'
+
+const getAll = async (): Promise<DiaryEntry[]> => {
+  const { data } = await axios.get<DiaryEntry[]>(
+    `${baseUrl}`
+  );
+
+  return data;
+};
+
+// const create = (newObject, token) => {
+//   const request = axios.post(baseUrl, newObject, {
+//     headers: { authorization: `Bearer ${token}` }
+//   })
+//   return request.then(response => response.data)
+// }
+
+// const update = (id, newObject) => {
+//   const request = axios.put(`${baseUrl}/${id}`, newObject)
+//   return request.then(response => response.data)
+// }
+
+// const createComment = (id, commentObject) => {
+//   const request = axios.post(`${baseUrl}/${id}/comments`, commentObject)
+//   return request.then(response => response.data)
+// }
+
+// const remove = (id, token) => {
+//   return axios.delete(`${baseUrl}/${id}`,{
+//     headers: { authorization: `Bearer ${token}` }
+//   })
+// }
+
+export default { getAll } //create, createComment,update, remove

af1d9df53a7ddf9fc2f82e2f3a375008221eec03
nicksndr <nicklas@marken-schneider.de>
2026-02-15 21:55:32 +0100

    exercise 9.21 setting up

--- part9/backend/types.ts
+++ part9/backend/types.ts
@@ -1,24 +1,31 @@
 export interface Diagnosis {
-    code: string;
-    name: string;
-    latin?: string;
-  }
+  code: string;
+  name: string;
+  latin?: string;
+}
 
-  export enum Gender {
-    Male = "male",
-    Female = "female",
-    Other = "other"
-  }
+export enum Gender {
+  Male = "male",
+  Female = "female",
+  Other = "other"
+}
 
-  export interface Patient {
-    id: string;
-    name: string;
-    occupation: string;
-    gender: Gender;
-    ssn?: string;
-    dateOfBirth?: string;
-  }
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface Entry {
+}
 
-  export type SensitivePatientData = Omit<Patient, "ssn">;
+export interface Patient {
+  id: string;
+  name: string;
+  occupation: string;
+  gender: Gender;
+  ssn?: string;
+  dateOfBirth?: string;
+  entries: Entry[];
+}
 
-  export type NewPatientData = Omit<Patient, 'id'>;
+export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
+
+export type SensitivePatientData = Omit<Patient, "ssn">;
+
+export type NewPatientData = Omit<Patient, 'id'>;

0fee540967c58d70a3c9ded315e8eb94211f3111
nicksndr <nicklas@marken-schneider.de>
2026-02-16 22:30:06 +0100

    exercise 9.21 add /api/patients/:id route to backend

--- part9/backend/src/routes/patients.ts
+++ part9/backend/src/routes/patients.ts
@@ -1,14 +1,27 @@
 import express from 'express';
 import patientsData from '../../data/patients';
 import patientService from '../services/patientService';
-import { SensitivePatientData } from '../../types';
+import { NonSensitivePatient, SensitivePatientData } from '../../types';
 import toNewPatientEntry from '../../utils';
 
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-    const patientsWithoutSsn: SensitivePatientData[] = patientsData.map(({ ssn, ...patient }) => patient);
-    res.send(patientsWithoutSsn);
+  // Destructure to exclude ssn; _ssn is intentionally unused 
+  const patientsWithoutSsn = patientsData.map(({ ssn: _ssn, ...patient }) => patient) as NonSensitivePatient[];
+  res.send(patientsWithoutSsn);
+});
+
+router.get('/:id', (req, res) => {
+  const patient = patientsData.find((p) => p.id === req.params.id);
+  if (!patient) {
+    res.sendStatus(404);
+    return;
+  }
+  // Destructure to exclude ssn from response (_ssn intentionally unused)
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars -- ssn omitted on purpose
+  const { ssn: _ssn, ...patientWithoutSsn } = patient;
+  res.json(patientWithoutSsn as SensitivePatientData);
 });
 
 router.post('/', (req, res) => {
